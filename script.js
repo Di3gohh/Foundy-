8f3aa5f4a2c0be82310fcf332b4edde7038b2f37
Di3gohh <diego.corazza9@gmail.com>
2026-03-30 18:39:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://127.0.0.1:8000/api';
+const API_URL = 'https://foundy-backend.onrender.com/api';
 let currentUser = null;
 let itensCadastrados = [];
 let isLoginMode = false;
